a66b2122cbe3a4f614f25f048d5e8daa76c5cbfd
Andrew Porter <port6555@csumb.edu>
2015-10-20 14:46:18 -0700

    finished Mel analysis engine, including animation frame conversion

--- TestFiles/pythontest.py
+++ TestFiles/pythontest.py
@@ -5,21 +5,19 @@ import os
 
 # function declarations
 #
-def scale(input):
-    input = (input - minVol) * scaleFactor
-    input = input * scaleFactor
-    return input
+
 
 # declare constants
 #
 frameRate = 24
 sampleRate = 1000 * frameRate
-
+melBins = 8
+sampleHop = 500
 
 # setup librosa functions/processing
 #
 y, sr = librosa.load("C:\\andre.mp3", sr=sampleRate)
-S = librosa.feature.melspectrogram(y=y, sr=sampleRate, n_mels=8,fmax=8000)
+S = librosa.feature.melspectrogram(y=y, sr=sampleRate, n_mels=melBins,fmax=8000,hop_length = sampleHop)
 librosaMel = librosa.logamplitude(S,ref_power=np.max)
 np.savetxt("C:\\newdata.dat", librosaMel)
 samples = len(librosaMel[1])
@@ -31,34 +29,37 @@ print(librosaMel)
 print("Samples: ")
 print(samples)
 print("Anim. Frames: ")
-frames = (frameRate * (samples * 512)) / sampleRate
+frames = (frameRate * (samples * sampleHop)) / sampleRate
 print(frames)
+print("Samples/Frame: ")
+print(float(samples)/float(frames))
 print("Seconds: ")
 print(frames/frameRate)
 
 # convert the bin based Mel spectrogram array to a time based array
 #	
-frameArry = []
+timeArray = []
 scaleFactor = 1.25
 
 for q in range(samples):
     tmpArry = []
-    for r in range(8):
+    for r in range(melBins):
 	tmpValue = ((librosaMel[r])[q])+80
         tmpArry.append(int(tmpValue*scaleFactor))
-    frameArry.append(tmpArry)
+    timeArray.append(tmpArry)
+np.savetxt("C:\\MelTimeArray.dat", timeArray)
 
-print("Frame Array Finished")
-np.savetxt("C:\\newdataArry.dat", frameArry)
+frameArray = []
+for q in range(samples/2):
+    tmpArry = []
+    for r in range(melBins):
+	tmpValue = (((timeArray[(2*q)])[r])+((timeArray[(2*q)+1])[r]))/2
+        tmpArry.append(int(tmpValue))
+    frameArray.append(tmpArry)
+np.savetxt("C:\\MelFrameArray.dat", frameArray)
 
-
-
-
-
-
-
-
-
+print("Frame Array Construction Finished")
+print("")
 
 print(stopscript)
 os.system("pause")
