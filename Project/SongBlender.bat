6f79d74d491b1aaba90bb88ff42c5385ef0a3083
Andrew Porter <port6555@csumb.edu>
2015-10-24 02:07:33 -0700

    Added modular CORE functionality to project

--- Project/SongBlender.bat
+++ Project/SongBlender.bat
@@ -19,6 +19,22 @@ ECHO.
 ECHO.
 ECHO What song would you like? (no extension)
 SET/P song="  ----> "
+CLS
+ECHO.
+ECHO.
+ECHO Here is a listing of render cores available:
+ECHO.
+ECHO.
+DIR .\Scripts\RenderingCores
+ECHO.
+ECHO.
+ECHO What core would you like? (no extension)
+SET/P core="  ----> "
+CLS
+ECHO.
+ECHO.
+ECHO For song %song%.mp3, rendered with core %core%:
+ECHO --------------
 ECHO.
 ECHO.
 ECHO How many elements would you like?
@@ -32,11 +48,12 @@ CD .\Scripts
 CALL python beat_frames.py %song%
 CALL python mel_volume.py %song%
 CALL python n_bin_mel.py %song% %bins% 
+CALL python core_insert.py %core%
 ECHO.
 ECHO Starting Blender.
 ECHO This console now belongs to Blender!
 ECHO.
-CALL blender empty.blend -P BlenderScript.txt %song% %frameskip%
+CALL blender empty.blend -P RenderScript.txt %song% %frameskip%
 ECHO.
 ECHO Blender is finished.
 ECHO This is now a system console!
